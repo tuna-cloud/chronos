5b0114d8b04287e3dd23283b2a09d09512edecf1
xshy <xshy@jiuyang.com>
2025-11-14 16:59:35 +0800

    meta data storage engine

--- chronos-server/src/main/java/org/apache/chronos/cluster/metastore/TagsIndexManager.java
+++ chronos-server/src/main/java/org/apache/chronos/cluster/metastore/TagsIndexManager.java
@@ -7,10 +7,11 @@ public class TagsIndexManager {
 
   private String indexFilePath;
   private String roaringBitmapFilePath;
-  private Map<String, TagsIndexManager> tagsIndexMap = Maps.newConcurrentMap();
+  private Map<String, TagsIndexEntry> tagsIndexMap = Maps.newConcurrentMap();
 
 
   public void init() {
     // 从磁盘读取索引，构建tagsIndexMap
+    // 索引文件有几个特点：仅追加和修改，不能物理删除释放空间
   }
 }
